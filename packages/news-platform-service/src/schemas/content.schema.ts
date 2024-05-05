8a8a8a894234048aec2dfe10ea788b83a9b4e91e
Yogesh Pawar <yogesh.pawar@ayanworks.com>
2024-05-05 21:24:42 +0530

    removed mongodb

--- packages/news-platform-service/src/schemas/content.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-
-@Schema()
-export class Content {
-  @Prop({ required: true })
-  content: string;
-}
-
-export const ContentSchema =  SchemaFactory.createForClass(Content)
