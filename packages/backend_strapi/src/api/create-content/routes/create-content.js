2bfeae67ddd98722691572c10cb92d8a18a54d41
Yogesh Pawar <yogesh.pawar@ayanworks.com>
2024-05-11 17:47:32 +0530

    feat/strapi-setup

---
+++ packages/backend_strapi/src/api/create-content/routes/create-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * create-content router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::create-content.create-content');
