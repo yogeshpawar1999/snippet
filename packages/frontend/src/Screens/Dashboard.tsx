0cd7299158d2e8a321f28d9a94f17e866df84dc5
Yogesh Pawar <yogesh.pawar@ayanworks.com>
2024-05-13 17:26:32 +0530

    feat: Updated dashboard

--- packages/frontend/src/Screens/Dashboard.tsx
+++ packages/frontend/src/Screens/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Navbar, Nav } from "react-bootstrap"
+import { Container, Row, Col, Navbar, Nav, Image } from "react-bootstrap"
 import Footer from "./layout/Footer"
 import { useC2pa, useThumbnailUrl } from "@contentauth/react"
 import { useState, useEffect, useRef } from "react"
@@ -16,11 +16,13 @@ import newsItems from "../newsItems"
 import Header from "./layout/header"
 
 function WebComponents({ imageUrl, provenance, viewMoreUrl }) {
+  console.log('Inside WebComponents');
+  
   const [manifestStore, setManifestStore] = useState(null)
   const summaryRef = useRef()
 
   useEffect(() => {
-    let disposeFn = () => {}
+    let disposeFn = () => { }
 
     if (!provenance.manifestStore?.activeManifest) {
       return
@@ -47,7 +49,16 @@ function WebComponents({ imageUrl, provenance, viewMoreUrl }) {
   return (
     <div className="web-components">
       <div className="wrapper">
-        <img src={imageUrl} />
+        {/* <img src={imageUrl} /> */}
+        {console.log('imageUrl', imageUrl)
+        }
+        <Image
+          src={imageUrl}
+          alt="Snippet News Logo"
+          width="100%"
+          height="100%"
+          className="me-3"
+        />
         {manifestStore ? (
           <div>
             <cai-popover interactive class="theme-spectrum">
@@ -65,17 +76,69 @@ function WebComponents({ imageUrl, provenance, viewMoreUrl }) {
   )
 }
 
+const MidComp = ({sampleImage}) => {
+  const provenance = useC2pa(sampleImage)
+  console.log("provenance h", provenance)
+  const viewMoreUrl = generateVerifyUrl(sampleImage)
+  console.log("viewMoreUrl", viewMoreUrl)
+  console.log('provenance?.manifestStore',sampleImage,  provenance?.manifestStore);
+  
+  return (
+    <>
+      {provenance?.manifestStore ?
+      <Col>
+        <WebComponents
+          imageUrl={sampleImage}
+          provenance={provenance}
+          viewMoreUrl={viewMoreUrl}
+        /> 
+        </Col>: null}
+    </>
+  )
+}
+
 const Dashboard = () => {
   // const sampleImage ="https://picsum.photos/id/0/5000/3333"
 
   const sampleImage =
     "https://raw.githubusercontent.com/contentauth/c2pa-js/main/tools/testing/fixtures/images/CAICAI.jpg"
   //   'https://raw.githubusercontent.com/contentauth/c2pa-js/main/tools/testing/fixtures/images/CAICAI.jpg';
+  // const provenance = [];
+  // provenance[0] = useC2pa(sampleImage)
+  // provenance[1] = useC2pa(sampleImage)
 
-  const provenance = useC2pa(sampleImage)
-  console.log("provenance", provenance)
-  const viewMoreUrl = generateVerifyUrl(sampleImage)
-  console.log("viewMoreUrl", viewMoreUrl)
+  // console.log("provenance", provenance)
+  // const viewMoreUrl = generateVerifyUrl(sampleImage)
+  // console.log("viewMoreUrl", viewMoreUrl)
+  const [newsList, setNewsList] = useState([]) 
+const API_KEY = '37ca773f6ef96b659beba80ce93739e02202b60c66544be1025b4bce433abef76196ab25b9087bc4e0c0ae87e710877417ffeba39a5beed9af7d3a46c54eca43fca5ef878861b45747f477d9961c0bf89b9a8f5d80e17170c3d6de8e935f4c9bdfda1cd19e5995c2272fda41ab6a95e2701451b4bca47596319fc7855558a918'
+  const getContentAPI = async () => {
+    fetch("http://localhost:1337/api/create-contents?populate=*", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const Dataele = data.data.map(ele=>{
+          return `http://localhost:1337${ele.attributes.ContentImages.data[0].attributes.url}`;
+        })
+        // setNewsList(()=> Dataele)
+        setNewsList((prevele)=> [...prevele, sampleImage, sampleImage, sampleImage, sampleImage])
+        console.log('Dataele', Dataele);
+        console.log('newsList', newsList);
+        
+        
+        // console.log("data", data.data.attributes)
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+  useEffect(()=>{
+    getContentAPI()
+  },[])
   return (
     <div className="App">
       <Header />
@@ -90,17 +153,26 @@ const Dashboard = () => {
             <hr />
             {/* News section */}
             <Row xs={4} md={3} lg={4} className="g-4">
-              {/* {provenance?.manifestStore ? (
-                <WebComponents
-                  imageUrl={sampleImage}
-                  provenance={provenance}
-                  viewMoreUrl={viewMoreUrl}
-                />
+              {console.log('newsList', newsList, Array.isArray(newsList), newsList.length)
+              }
+              
+              {newsList?.length && newsList?.map(item=> 
+            <MidComp sampleImage={sampleImage} />)}
+              {/* {provenance[0]?.manifestStore ? (
+                newsItems.map((item, index) => (
+                  // {provenance[0]?.manifestStore ? (
+                  <WebComponents
+                    imageUrl={sampleImage}
+                    provenance={provenance[0]}
+                    viewMoreUrl={viewMoreUrl}
+                  />
+                  // ) : null}
+                ))
               ) : null} */}
               {/* Loop through news items and render cards */}
-              {newsItems.map((item) => (
+              {/* {newsItems.map((item) => (
                 <NewsCard key={item.id} imageSrc={item.imageSrc} />
-              ))}
+              ))} */}
             </Row>
           </Col>
           {/* Rest of your layout code remains the same */}
